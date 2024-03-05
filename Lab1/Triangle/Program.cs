f0d2308db1a07941396d0bda320f1ab1d8bf5af5
DmitryIldyukov <dmitry.ildyukov@mail.ru>
2024-03-05 17:05:03 +0300

    Добавил 1 лабу

---
+++ Lab1/Triangle/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Triangle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 3)
+        {
+            Console.WriteLine("Неправильный ввод");
+            return;
+        }
+        List<double> verticals = new List<double>();
+
+        foreach (var arg in args)
+        {
+            string num = arg.Replace(".", ",");
+            if (double.TryParse(num, out var @_))
+            {
+                verticals.Add(_);
+            }
+            else
+            {
+                Console.WriteLine($"{arg} не является числом");
+                return;
+            }
+        }
+        
+        Console.WriteLine(GetTriangleType(verticals));
+    }
+
+    private static string GetTriangleType(List<double> verticals)
+    {
+        if (verticals.Count != 3)
+            return "Неправильный ввод";
+            
+        if (verticals.Any(_ => _ <= 0) || !TriangleCanExist(verticals[0], verticals[1], verticals[2]))
+            return "Не треугольник";
+        if (verticals[0] == verticals[1] && verticals[0] == verticals[2])
+            return "Равносторонний";
+        if (verticals[0] == verticals[1] || verticals[0] == verticals[2] || verticals[1] == verticals[2])
+            return "Равнобедренный";
+        return "Обычный";
+    }
+
+    private static bool TriangleCanExist(double a, double b, double c) => ((a + b > c) && (a + c > b) && (b + c > a));
+}
---
+++ Lab1/TriangleTests/TriangleTests/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace TriangleTests;
+
+class Program
+{
+    private const string path = "tests.txt";
+    
+    static void Main()
+    {
+        try
+        {
+            string[] tests = File.ReadAllLines(path);
+            foreach (var test in tests)
+            {
+                string[] allArguments = test.Split(' ');
+                string expectedAnswer = allArguments[allArguments.Length - 1];
+                List<string> arguments = allArguments.ToList();
+                if (arguments.Count > 0)
+                    arguments.RemoveAt(arguments.Count - 1);
+                string argument = string.Join(' ', arguments);
+                string relativePath = "Triangle.exe";
+                string currentDirectory = Directory.GetCurrentDirectory();
+                string absolutePath = Path.Combine(currentDirectory, relativePath);
+                Process process = new Process();
+                process.StartInfo.FileName = absolutePath;
+                process.StartInfo.Arguments = argument;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                Console.WriteLine(output.ToLower().Replace(" ", string.Empty).Replace("\r\n", string.Empty) == expectedAnswer
+                    ? "success"
+                    : "error");
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Файл не найден!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Произошла ошибка при считывании с файла: {e}");
+        }
+    }
+}
